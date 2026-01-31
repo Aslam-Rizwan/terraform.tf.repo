6fe41039756444117874b23d2d8595fefbb2e01c
Aslam-Rizwan <aslamr196@gmail.com>
2026-01-31 16:46:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,7 @@
-# resource "azurerm_resource_group" "rg45"{
-#   for_each=var.rg_name
-#   name     = each.value.name
-#   location = each.value.location
-# }
-# variable "rg_name"{
- 
-# }
-
-variable "rg_name"{}
-resource "azurerm_resource_group" "rg42" {
-  for_each= var.rg_name
-  name     = each.key
-  location = each.value
+resource "azurerm_resource_group" "rg45"{
+for_each=var.rg_name
+  name     = each.value.name
+  location = each.value.location
 }
+variable "rg_name"{ }
+
